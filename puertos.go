7b68c717858a800e095921fe8e1d597543043ee9
Dar'em <48230857+Lecc312@users.noreply.github.com>
2019-11-18 19:36:30 -0600

    Update puertos.go

--- puertos.go
+++ puertos.go
@@ -24,17 +24,16 @@ func solicitud(ip string, min int, max int) {
 
 func main(){
 	var ip="google.com.mx"
-	
 	t := time.Now()
 	fecha := fmt.Sprintf("%02d:%02d:%02d",t.Hour(), t.Minute(), t.Second())
 	fmt.Println(fecha)
 	go solicitud(ip,0,13107)
-	//go solicitud(ip,13107,26214)
-	//go solicitud(ip,26214,39321)
-	//go solicitud(ip,39321,52427)
-	//go solicitud(ip,52427,65536)
-	//go solicitud(ip,80,101)
+	go solicitud(ip,13107,26214)
+	go solicitud(ip,26214,39321)
+	go solicitud(ip,39321,52427)
+	go solicitud(ip,52427,65536)
 	<- c
-	fecha = fmt.Sprintf("%02d:%02d:%02d",t.Hour(), t.Minute(), t.Second())
-	fmt.Println(fecha)
+	t = time.Now()
+	fechafin := fmt.Sprintf("%02d:%02d:%02d",t.Hour(), t.Minute(), t.Second())
+	fmt.Println(fechafin)
 }
